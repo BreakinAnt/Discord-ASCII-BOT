24246b3c8d5ea6cb8f514b5c24787350c97f6bbe
Felipe Carneiro <flipecar97@gmail.com>
2019-09-26 13:36:58 -0300

    callbacks implemented
    Callbacks implemented for (hopefully) a better code readability.

--- bot.js
+++ bot.js
@@ -22,6 +22,18 @@ bot.on('ready', function (evt) {
     console.log("***Bot has logged in***");
 });
 
+//connecting with our mysql server
+var con = mysql.createConnection({
+    host: 'host',
+    user: 'user',
+    password: 'password',
+   });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("***Database has logged in***");  
+  });  
+
 //receiving message
 bot.on('message', function (user, userID, channelID, message, evt) {
     //it will listen for messages that will start with `!`
@@ -47,21 +59,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 asciiHelp(channelID);
             break;
         }                          
-     }     
+    }     
 }); 
 //
-
-//connecting with our mysql server
-var con = mysql.createConnection({
-  host: 'host',
-  user: 'user',
-  password: 'password',
- });
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("***Database has logged in***");  
-});
 
 //--- chat commands ---
 function asciiAdd(user, channelID, params){
@@ -72,24 +72,46 @@ function asciiAdd(user, channelID, params){
             message: "Adding...\n"
             
         });
-    sqlInsert(params, user, channelID); 
-	
-        } else {
+		
+        sqlInsert(params, user, message => {
             bot.sendMessage({
-            
+                
                 to: channelID,
-                message: "No ASCII found."
+                message: message
             
             });
-        }
+        }); 
+	
+    } else {
+        bot.sendMessage({
+            
+            to: channelID,
+            message: "No ASCII found."
+            
+        });
+    }
 }
 
 function asciiGet(channelID, params){ 
     if (params != null && params >= 0) {         
-        asciiSelect(params, channelID);
-            } else {                               
-            asciiSelectRandom(channelID);
-            }
+        asciiSelect(params, message => {
+            bot.sendMessage({
+            
+                to: channelID,
+                message: message
+            
+            });   
+        });
+    } else {                               
+        asciiSelectRandom(message => {
+			bot.sendMessage({
+       
+            to: channelID,
+            message: message
+                
+            });
+        });
+    }
 }
 
 function asciiHelp(channelID){
@@ -103,90 +125,59 @@ function asciiHelp(channelID){
     });    
 }
 
-//--- mysql ---
 //insert ascii
-function sqlInsert(art, name, channelID){    
+function sqlInsert(art, name, callback){    
     con.query("SELECT arte_ascii, id_ascii FROM pOwQ5cpo4x.tb_ascii WHERE BINARY '"+art+"' = BINARY arte_ascii", function (err, result) {                              
   
-        var comparationArt;   
+        let comparationArt;   
           
         if (result != ""){
-            comparationArt = result[0].arte_ascii;
-                       
-            } else {
-                comparationArt = null;
-            }
+            comparationArt = result[0].arte_ascii;                       
+		} else {
+            comparationArt = null;
+        }
 			
         //check to see if art(ascii user has inputed) is equal to comparationArt(ascii found on our database) before adding it
         if (charReplace(art) == comparationArt) {            
-            bot.sendMessage({
-                    
-                to: channelID,
-                message: `ASCII already exists by ID: ${result[0].id_ascii}`
-                
-            });            
-            } else {              
-                asciiInsert(art, name); 				
-                //returns the ascii's ID that we just added to our database            
-                con.query("SELECT id_ascii FROM pOwQ5cpo4x.tb_ascii WHERE BINARY '"+art+"' = BINARY arte_ascii", function (err, result) {                
-                    if (err) throw err;
-                    var id = result[0].id_ascii;
-                    
-                    bot.sendMessage({
-                        
-                        to: channelID,                
-                        message: `User ${name} has added:\n${charOriginal(art)}\nID: ${id}`
-                        
-                    });                
-                });              
-            }                  
+                callback(`ASCII already exists by ID: ${result[0].id_ascii}`);   
+        } else {
+            asciiInsert(art, name); 				
+            //returns the ascii's ID that we just added to our database            
+            con.query("SELECT id_ascii FROM pOwQ5cpo4x.tb_ascii WHERE BINARY '"+art+"' = BINARY arte_ascii", function (err, result) {                
+                if (err) throw err;
+                callback(`User ${name} has added:\n${charOriginal(art)}\nID: ${result[0].id_ascii}`);            
+            });              
+        }                  
     });     
 
-    function asciiInsert(art, name){
- 
-    var sql = "INSERT INTO pOwQ5cpo4x.tb_ascii (arte_ascii, nome_ascii, data_ascii) VALUES ( '"+art+"', '"+name+"', CURDATE())";
-    con.query(sql, function (err) {
-        if (err) throw err;        
-      }); 
+    function asciiInsert(art, name){ 
+        let sql = "INSERT INTO pOwQ5cpo4x.tb_ascii (arte_ascii, nome_ascii, data_ascii) VALUES ( '"+art+"', '"+name+"', CURDATE())";
+        con.query(sql, function (err) {
+            if (err) throw err;        
+        }); 
     }
 }
 
 //select specific id
-function asciiSelect(id, channelID){
+function asciiSelect(id, callback){
     con.query("SELECT * FROM pOwQ5cpo4x.tb_ascii WHERE "+id+" = id_ascii", function (err, result) {
-        //if our variable result has a [object Object] it means we found our ascii               
-        if (result == "[object Object]"){
-                
-            bot.sendMessage({
-                    
-                to: channelID,
-                message: `ID: ${result[0].id_ascii}\n${charOriginal(result[0].arte_ascii)}`                
-                
-            });                                 
-            } else {            
-                bot.sendMessage({
-                        
-                    to: channelID,
-                    message: "No ASCII found."
-                    
-                });
-            }        
-        });
+        
+        //check to see if it has found an ascii     
+        if (result != ""){
+                callback(`ID: ${result[0].id_ascii}\n${charOriginal(result[0].arte_ascii)}`);                                                                        
+        } else {
+            callback("No ASCII found.");
+        }        
+    });
 }
 
 //select and picking a random one from the list
-function asciiSelectRandom (channelID){
+function asciiSelectRandom (callback){
     con.query("SELECT * FROM pOwQ5cpo4x.tb_ascii", function (err, result) {
         if (err) throw err;
-        var idRandom = Math.random() * result.length;
+        let idRandom = Math.random() * result.length;
         idRandom = parseInt(idRandom ++, 10);
-        
-        bot.sendMessage({
-
-            to: channelID,
-            message: `ID: ${result[idRandom].id_ascii}\n${charOriginal(result[idRandom].arte_ascii)}`
-            
-        });                                                                
+        callback(`ID: ${result[idRandom].id_ascii}\n${charOriginal(result[idRandom].arte_ascii)}`);                                                                          
     });
 }
 
